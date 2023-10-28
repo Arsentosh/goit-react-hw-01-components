6b40058bb9b0ad83082234327d983f931af6ff4b
Arsentosh <doncenkoarsenij185@gmail.com>
2023-10-28 20:50:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
+const jacques = {
+  username: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const Profile = props => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src={props.avatar}
+          alt={props.username}
+          class="avatar"
+          width={120}
+          height={120}
+        />
+        <p class="name">{props.username}</p>
+        <p class="tag">{props.tag}</p>
+        <p class="location">{props.location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{props.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{props.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{props.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+      style={
+        {
+          // // height: '100vh',
+          // display: 'flex',
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          // fontSize: 40,
+          // color: '#010101',
+        }
+      }
     >
-      goit-react-hw-01-components
+      <Profile
+        username={jacques.username}
+        tag={jacques.tag}
+        location={jacques.location}
+        avatar={jacques.avatar}
+        stats={jacques.stats}
+      />
     </div>
   );
 };
